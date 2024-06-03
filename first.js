fa332f21f7e0cb5e25c73b44dc25f754cf79b422
Laura Irene Capo <144149179+lcapohernandez@users.noreply.github.com>
2024-06-03 15:07:11 -0400

    Add files via upload
    First commit

---
+++ first.js
@@ -0,0 +1,11 @@
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let generateExcuse = () => {
+    return excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
+}
+
+console.log(generateExcuse());
